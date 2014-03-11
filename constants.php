7dc3ec8d9995c230747c5e58de6f5d60c49545fc
Alejandro Vera <alejandro.vera@gmail.com>
2014-03-11 18:14:40 -0300

    mejoras a la configuracion

--- constants.php
+++ constants.php
@@ -4,4 +4,4 @@ define('SECRET', '420089d5908bcdccac45366d4e3795924679290f');
 define('KHIPU_URL', 'https://khipu.com/api/1.2/');
 define('BASE_DIRECTORY', 'http://demo.khipu.com/demojs/');
 define('RETURN_URL', BASE_DIRECTORY . 'finish.php');
-define('NOTIFY_URL', 'http://demo.khipu.com/demo-notify-js.php');
+define('NOTIFY_URL', BASE_DIRECTORY . 'demo-notify-js.php');
